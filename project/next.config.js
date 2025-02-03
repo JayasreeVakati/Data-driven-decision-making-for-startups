2131ba47eb9bebf3232062b80a94712f9412c481
JayasreeVakati <VakatiJayasree93@gmail.com>
2025-02-03 20:08:32 +0530

    Update next.config.js

--- project/next.config.js
+++ project/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
+  images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
